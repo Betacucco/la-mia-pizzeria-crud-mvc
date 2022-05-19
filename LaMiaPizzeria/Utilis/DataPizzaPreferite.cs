a1952265d0c5eb198760021f6d9182e6f29b27e7
MarcoPieretti <marcopieretti50@gmail.com>
2022-05-19 17:42:12 +0200

    Bonus non funzionante

---
+++ LaMiaPizzeria/Utilis/DataPizzaPreferite.cs
@@ -0,0 +1,24 @@
+﻿using LaMiaPizzeria.Models;
+
+namespace LaMiaPizzeria.Utilis
+{
+    public static class DataPizzaPreferite
+    {
+        private static List<Pizza>? pizze;
+
+        public static List<Pizza> GetPizze()
+        {
+            if (DataPizzaPreferite.pizze != null)
+            {
+                return DataPizzaPreferite.pizze;
+            }
+
+            List<Pizza> listaPizzePreferite = new List<Pizza>();
+
+            DataPizzaPreferite.pizze = listaPizzePreferite;
+
+            return DataPizzaPreferite.pizze;
+
+        }
+    }
+}
