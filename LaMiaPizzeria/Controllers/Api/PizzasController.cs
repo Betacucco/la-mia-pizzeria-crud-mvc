af30c7fa44c08b9ddbdae65c1c2d41b778dcd5f5
MarcoPieretti <marcopieretti50@gmail.com>
2022-05-19 14:55:03 +0200

    Primo push senza bonus

---
+++ LaMiaPizzeria/Controllers/Api/PizzasController.cs
@@ -0,0 +1,24 @@
+﻿using LaMiaPizzeria.Data;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = new List<Pizza>();
+
+            using(PizzeriaContext db = new PizzeriaContext())
+            {
+                pizze = db.Pizze.ToList<Pizza>();
+            }
+            return Ok(pizze);
+        }
+    }
+}
